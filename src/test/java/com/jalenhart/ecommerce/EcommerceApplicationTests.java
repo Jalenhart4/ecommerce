d2f85cfe241ed557efbff1d0fbfd93c43eda13d3
Jalen Terrell-Hart <Jalenhart4@gmail.com>
2022-12-13 10:42:44 -0600

    Adding Files

---
+++ src/test/java/com/jalenhart/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalenhart.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
